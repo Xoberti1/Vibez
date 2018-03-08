6ef0b820d7646ad91e0dd731cec14b3a0eabb889
Xoberti1 <xoberti@gmail.com>
2018-03-07 20:55:26 -0600

    precursor

--- backEnd/Routes/signin.js
+++ backEnd/Routes/signin.js
@@ -1,7 +1,7 @@
 app.post('/login',
-    passport.authenticate('local', {
-        successRedirect: '/',
-        failureRedirect: '/login',
-        failureFlash: true
-    })
-);
+    passport.authenticate('local'),
+    function (req, res) {
+        // If this function gets called, authentication was successful.
+        // `req.user` contains the authenticated user.
+        res.redirect('/users/' + req.user.username);
+    });
