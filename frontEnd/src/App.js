87da469ab5fc93200c6a30f17960820b42917f2c
bhoang23 <bhoang02@gmail.com>
2018-03-07 20:44:02 -0600

    added registration files

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -1,23 +1,23 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Signin from "./pages/signin/Signin";
-import concerts from "./pages/concerts";
-import chatroom from "./pages/chatroom";
-import register from "./pages/register";
-import Navbar from "./components/Navbar";
-import Wrapper from "./components/Wrapper";
-import Footer from "./components/Footer";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+// import Signin from "./pages/signin/Signin";
+// import concerts from "./pages/concerts";
+// import chatroom from "./pages/chatroom";
+import register from "./pages/registration/registrationValidation";
+// import Navbar from "./components/Navbar";
+// import Wrapper from "./components/Wrapper";
+// import Footer from "./components/Footer";
 
 const App = () =>
   <Router>
     <div>
       <Navbar />
       <Wrapper>
-        <Route exact path="/" component={Signin} />
+        {/* <Route exact path="/" component={Signin} />
         <Route exact path="/signin" component={Signin} />
         <Route exact path="/concerts" component={Concerts} />
-        <Route exact path="/chatroom" component={Chatroom} />
-        <Route exact path="/register" component={Register} />
+        <Route exact path="/chatroom" component={Chatroom} /> */}
+        <Route exact path="/registrationValidation" component={Register} />
       </Wrapper>
       <Footer />
     </div>
--- frontEnd/src/pages/registration/registration.js
+++ frontEnd/src/pages/registration/registration.js
@@ -1,1 +1,21 @@
 import React, {Component} from "react";
+
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { Values } from "redux-form-website-template";
+import store from "./store";
+import results from "./results";
+import registration from "./registration";
+
+const rootEl = document.getElementById("root");
+
+ReactDOM.render(
+  <Provider store={store}>
+    <div style={{ padding: 15 }}>
+      <h2>Field-Level Validation</h2>
+      <registration onSubmit={showResults} />
+      <Values form="registration" />
+    </div>
+  </Provider>,
+  rootEl
+);
