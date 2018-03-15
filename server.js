4271e8bdec3bd78876fe395a058fafa6c3fd54ad
Xoberti1 <xoberti@gmail.com>
2018-03-14 19:36:30 -0500

    chris minor changes

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ const PORT = process.env.PORT || 3001;
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-// Serve up static assets
-app.use(express.static("client/build"));
+if(process.env.NODE_ENV === "production"){
+    // Serve up static assets
+    app.use(express.static("client/build"));
+}
 // Add routes, both API and view
 app.use(routes);
-app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/client/build/index.html")
-});
+
 app.get("*", function(req, res){
     res.sendFile(__dirname + "/client/build/index.html")
 });
